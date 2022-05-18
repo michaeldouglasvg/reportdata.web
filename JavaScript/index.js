ced0e8bd03110b321e735cce998e94bf0ef7e807
michaeldouglasvg <douglas1999michael@gmail.com>
2022-05-18 19:37:59 +0300

    port

---
+++ JavaScript/index.js
@@ -0,0 +1,32 @@
+// filter js
+
+const aboutContainer = document.querySelector(".article");
+const aboutBtns = document.querySelectorAll(".btn");
+const contentAbout = document.querySelectorAll(".article__display-sec");
+
+aboutContainer.addEventListener("click", (e) =>{
+
+ const currentId = e.target.dataset.about;
+
+ if(currentId){
+
+  aboutBtns.forEach((btn) => {
+   // removing active class for all btns and adding to current target btn
+   btn.classList.remove("active");
+   e.target.classList.add("active");
+
+
+   // removing active class from all the articles
+   contentAbout.forEach((content) => {
+
+    content.classList.remove("active")
+
+   });
+
+   const currentMatchId = document.getElementById(currentId);
+   currentMatchId.classList.add("active");
+
+  });
+ }
+
+});
